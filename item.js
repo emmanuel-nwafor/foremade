893020c83b801053b4097df11c194106bef4b5cc
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-04-21 01:24:59 -0700

    all commit

--- item.js
+++ item.js
@@ -1,9 +1,9 @@
 const items = [
     {
+        image: "https://foremade.com/storage/app/public/product/thumbnail/2025-04-13-67fb3a207ba51.webp",
         stars: [],
-        image: ,
-        name: ,
-        company: ,
-        price: ,
+        company: "Foremade",
+        name: "VIZIO 55” 4K",
+        price: "500,000.00",
     }
 ]
