3bd158cddc56140f6a717be756072c82fea36fc7
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-04-21 01:30:39 -0700

    all commit

--- item.js
+++ item.js
@@ -4,6 +4,41 @@ const items = [
         stars: [],
         company: "Foremade",
         name: "VIZIO 55” 4K",
-        price: "500,000.00",
+        price: "500000.00",
+    },
+    {
+        image: "https://foremade.com/storage/app/public/product/thumbnail/revive-placeholder.webp",
+        stars: [],
+        company: "Hynolyn Health Servi...",
+        name: "Revive",
+        price: "43000.00",
+    },
+    {
+        image: "https://foremade.com/storage/app/public/product/thumbnail/vigor-essential-placeholder.webp",
+        stars: [],
+        company: "Hynolyn Health Servi...",
+        name: "Vigor Essential",
+        price: "24000.00",
+    },
+    {
+        image: "https://foremade.com/storage/app/public/product/thumbnail/memory-247-placeholder.webp",
+        stars: [],
+        company: "Hynolyn Health Servi...",
+        name: "Memory 24/7",
+        price: "32900.00",
+    },
+    {
+        image: "https://foremade.com/storage/app/public/product/thumbnail/ginseng-coffee-placeholder.webp",
+        stars: [],
+        company: "Hynolyn Health Servi...",
+        name: "Ginseng Coffee",
+        price: "28000.00",
+    },
+    {
+        image: "https://foremade.com/storage/app/public/product/thumbnail/colon-cleanser-tea-placeholder.webp",
+        stars: [],
+        company: "Hynolyn Health Servi...",
+        name: "Colon Cleanser Tea",
+        price: "TBD",
     }
-]
+];
